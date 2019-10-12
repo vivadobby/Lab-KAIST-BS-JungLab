eade1fc7ada7a2a3e0758e808a20afc9553699f9
Juan Lee <juanlee@kaist.ac.kr>
2019-10-13 00:37:55 +0900

    Add initial controller

---
+++ dobby/index.js
@@ -0,0 +1,11 @@
+/*
+    Update index.html
+
+    Written by Juan Lee <juanlee@kaist.ac.kr>
+*/
+
+let updateIndex = () => {
+  readSheet("1SKorZHjK71eDo45vy1VjTgnaGVxjVOea58bMrwClb3w").then(sheets => {
+    console.log(sheets);
+  });
+};
---
+++ dobby/team.js
@@ -0,0 +1,11 @@
+/*
+    Update team.html
+
+    Written by Juan Lee <juanlee@kaist.ac.kr>
+*/
+
+let updateTeam = () => {
+  readSheet("1SKorZHjK71eDo45vy1VjTgnaGVxjVOea58bMrwClb3w").then(sheets => {
+    console.log(sheets);
+  });
+};
